f7daa3231ff6e4def52834775fb63103cb95eb13
Waqas Obeidy <WObeidy@LT-Waqas-Obeidy.local>
2023-05-06 12:43:33 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks import get_openai_callback
+from streamlit_chat import message as st_message
+
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Ask your Document 💬")
+    st.header("Ask your Document 💬")
+    if "history" not in st.session_state:
+      st.session_state.history = []
+
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      
+      # create embeddings
+      embeddings = OpenAIEmbeddings()
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+      # show user input
+      user_question = st.text_input("Ask a question about your PDF:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+        
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.1)
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
+        
+        st.session_state.history.append({"message": user_question, "is_user": True})
+        st.session_state.history.append({"message": response, "is_user": False})
+
+        for i, chat in enumerate(st.session_state.history):
+          st_message(**chat, key=str(i))
+
+if __name__ == '__main__':
+    main()
