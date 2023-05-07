c4eb89ac22cec9975c8adede2433e98d5dafe8ef
Waqas Obeidy <WObeidy@LT-Waqas-Obeidy.local>
2023-05-07 16:13:26 +0800

    added streaming import

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-# from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import get_openai_callback
